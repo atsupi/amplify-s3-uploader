186cd22983ee65eba5fcf32a2b2c5c4d331f033b
atsupi <atsupi@gmail.com>
2023-01-03 00:16:09 +0900

    Added amplify storage.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "amplifys3uploader": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }

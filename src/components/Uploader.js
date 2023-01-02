b0de3e193d09416ee35b554f5fc6e6f3a2c6f913
atsupi <atsupi@gmail.com>
2023-01-03 00:17:18 +0900

    Added video file uploading function.

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,10 +1,55 @@
 import './Uploader.css'
+import { Storage } from 'aws-amplify'
+import { useState } from 'react'
+
+async function getPresignedUrl (key) {
+    const presignedUrl = await Storage.get(key);
+    console.log(presignedUrl);
+    return presignedUrl;
+}
 
 function Uploader () {
+
+    const [presignedUrl, setPresignedUrl] = useState("");
+    const [dataName, setDataName] = useState("No data set");
+    const [isUrlSet, setIsUrlSet] = useState(false);
+
+    const inputFile = (event) => {
+        console.log(event.target.files[0]);
+        if (event.target.files[0].size < 20000000) {
+            console.log("Storage.put");
+            console.log(event.target.files[0].name);
+            console.log(event.target.files[0].type);
+            const upload = Storage.put(event.target.files[0].name, event.target.files[0], {
+                level: "public", 
+                type: event.target.files[0].type,
+                progressCallback(progress) {
+                    let percentage = 100 * progress.loaded / progress.total;
+                    console.log(`progress: ${percentage}%`);
+                }
+            }).then(result => {
+                console.log(`Completed the upload of ${result.key}`);
+                console.log(event.target.files[0].name);
+                const presignedUrl2 = getPresignedUrl(event.target.files[0].name)
+                  .then(result => {
+                    setPresignedUrl(result);
+                    setDataName(event.target.files[0].name);
+                    setIsUrlSet(true);
+                  });
+            }).catch(event => {
+                console.log("caught event")
+                console.log(event);
+            })
+        }
+    }
+
     return (
         <>
         <div className='Uploader'>
-            Uploader
+            <input type="file" onChange={inputFile} />
+        </div>
+        <div>
+            {(isUrlSet)? <video controls serc={presignedUrl} /> : `No data set`}
         </div>
         </>
     )

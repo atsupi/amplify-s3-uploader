e539996e52c16b29ef5320b6c15086584d09b06a
atsupi <atsupi@gmail.com>
2023-01-03 03:18:48 +0900

    Added source tag in HTML5 video playback.

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -49,7 +49,7 @@ function Uploader () {
             <input type="file" onChange={inputFile} />
         </div>
         <div>
-            {(isUrlSet)? <video controls src={presignedUrl}><p>Video playback is not supported</p></video> : `No data set`}
+            {(isUrlSet)? <video controls><source src={presignedUrl}></video> : `No data set`}
         </div>
         </>
     )

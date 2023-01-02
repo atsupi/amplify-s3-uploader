8c013756ec91aa2128e7b1bed91db89f9ee5f61a
atsupi <atsupi@gmail.com>
2023-01-03 00:58:28 +0900

    Refreshed amplify resources and added hosting.

--- amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "amplifys3uploaderea3e05c3": {
+        "amplifys3uploader8aabda12": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
@@ -14,18 +14,6 @@ export type AmplifyDependentResourcesAttributes = {
         "amplifys3uploader": {
             "BucketName": "string",
             "Region": "string"
-        }
-    },
-    "hosting": {
-        "S3AndCloudFront": {
-            "Region": "string",
-            "HostingBucketName": "string",
-            "WebsiteURL": "string",
-            "S3BucketSecureURL": "string",
-            "CloudFrontDistributionID": "string",
-            "CloudFrontDomainName": "string",
-            "CloudFrontSecureURL": "string",
-            "CloudFrontOriginAccessIdentity": "string"
         }
     }
 }
